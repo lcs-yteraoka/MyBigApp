02c175c17afeb29773eee584da79c84fdcdfa888
Yahiro Teraoka <yteraoka@lcs.on.ca>
2026-03-01 13:09:22 -0500

    Initial Commit

---
+++ MyBigApp/MyBigAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBigAppApp.swift
+//  MyBigApp
+//
+//  Created by Yahiro Teraoka on 2026-03-01.
+//
+
+import SwiftUI
+
+@main
+struct MyBigAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
